121c8f719f4083201d400aac4a988da9914fbd94
RatoX <rodrigovieira18@gmail.com>
2014-12-17 19:43:39 -0300

    Simplify using merge method from Hash.

--- lib/jekyll-paginate-by/pagination.rb
+++ lib/jekyll-paginate-by/pagination.rb
@@ -2,8 +2,9 @@ module Jekyll
   module PaginateBy
     class Pagination < Generator
       attr_reader :site
-      # This generator is safe from arbitrary code execution.
+      PAGINATION_DISABLED = 'Pagination is enabled, but I couldn\'t find an layout page to use as the pagination template, please site.\'paginate_by\' in config. Skipping pagination.'
 
+      # This generator is safe from arbitrary code execution.
       safe true
 
       # This generator should be passive with regard to its execution
@@ -17,31 +18,23 @@ module Jekyll
 
       def generate(site)
         if Pager.pagination_enabled?(site)
-          if configs =  site.config['paginate_by']
-            configs = Pagination.parse_config(configs)
-            configs["filters"].each do |config|
-              site.pages +=PaginationGenerator.new(config, site).process
+          if configs = site.config['paginate_by']
+            Pagination.parse_config(configs).each do |config|
+              site.pages += PaginationGenerator.new(config, site).process
             end
           else
-            Jekyll.logger.warn "Pagination:", "Pagination is enabled, but I couldn't find " +
-              "an layout page to use as the pagination template, please site.'paginate_by' in config. Skipping pagination."
+            Jekyll.logger.warn 'Pagination:', PAGINATION_DISABLED
           end
         end
       end
 
       def self.parse_config(configs)
-        options = configs["options"]
-        root_keys = Array.new(options.keys)
-        configs["filters"].each do |filter|
-          filter.keys.each do |filter_key_name|
-            root_keys.each do |key|
-              unless filter[filter_key_name].has_key? key
-                filter[filter_key_name][key] = options[key]
-              end
-            end
-          end
+        options = configs['options']
+        configuration_filters = []
+        configs['filters'].each do |filter|
+          configuration_filters << options.merge(filter)
         end
-        configs
+        configuration_filters
       end
     end
   end
